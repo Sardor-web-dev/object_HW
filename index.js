b096631195796b18da1f47dd2a580010208be5e4
SarikViper <dzamolovsardor5@gmail.com>
2024-12-23 22:54:18 +0500

    first commit

--- index.js
+++ index.js
@@ -10,23 +10,7 @@ let first = arr[0][0].a.price
 
 let second = arr[1][0].a.price
 
-let third = arr[2][0].a.price
 
-let total = 0
-
-total = first + second + third
-
-console.log(total);
-
-
-
-//Второе задание
-
-let students = [
-    { name: "Ali", age: 18, grades: [90, 95, 85] },
-    { name: "Leyla", age: 19, grades: [80, 75, 85] },
-    { name: "Omar", age: 20, grades: [100, 95, 90] }
-];
 
 
 const average = grades => grades.reduce((sum, grade) => sum + grade, 0) / grades.length;
@@ -35,8 +19,6 @@ const average = grades => grades.reduce((sum, grade) => sum + grade, 0) / grades
 const topStudent = students.reduce((top, student) => 
     average(student.grades) > average(top.grades) ? student : top
 );
-
-
 students.forEach(student => {
     student.status = average(student.grades) >= 90 ? "Отличник" : "Хорошист";
     console.log(`Имя: ${student.name}, Статус: ${student.status}`);
